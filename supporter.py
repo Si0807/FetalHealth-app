3c4a878a866ec9f6b775f89ba85701f8da93f204
Sid08 <sidharthsivadas02@gmail.com>
2025-06-20 13:12:11 +0300

    Add files via upload

---
+++ supporter.py
@@ -0,0 +1,22 @@
+deleted_list=['fetal_movement','light_decelerations','histogram_tendency','histogram_number_of_zeroes','histogram_number_of_peaks',
+              'histogram_max','histogram_min','histogram_width','severe_decelerations','mean_value_of_short_term_variability']
+values=[baseline,
+            accelerations,
+            uterine_contractions,
+            prolonged_decelerations,
+            abnormal_short_term_variability,
+            percentage_of_time_with_abnormal_long_term_variability,
+            histogram_mode,
+            histogram_mean,
+            histogram_median,
+            histogram_variance
+            ]
+
+
+['baseline value', 'accelerations', 'uterine_contractions',
+       'prolongued_decelerations', 'abnormal_short_term_variability',
+       'percentage_of_time_with_abnormal_long_term_variability',
+       'mean_value_of_long_term_variability', 'histogram_mode',
+       'histogram_mean', 'histogram_median', 'histogram_variance',
+       'fetal_health']
+
